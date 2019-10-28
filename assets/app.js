2d98b4b5870df5e9a598156eefb98f93062965ac
Ziad Eldesouky <Ziad.dessoki@yahoo.com>
2019-10-28 00:02:28 -0400

    done with 99% of js

--- assets/app.js
+++ assets/app.js
@@ -1,31 +1,41 @@
 $(document).ready(function () {
-
+   
     var tags = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King"];
-
+    $(document).on("click",".w",searchGiphy);
+    addButton();
+    searchGiphy();
 
       function searchGiphy() {
+        $("#images").empty();
          var tag = $(this).attr("data-name");
-        var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=OMySCUA9XjxGmOYiuVoKIf0iKdmJooeA&q=' + tag + '&limit=25&offset=0&rating=R&lang=en';
-
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=OMySCUA9XjxGmOYiuVoKIf0iKdmJooeA&q=' + tag + '&limit=10&offset=0&&lang=en';
+        
         $.ajax({ 
             url: queryURL, 
             method: "GET" 
         }).then(function (response) {
-            console.log(response);
+            // console.log(response);
             var results = response.data;
-
-
+            
+            
            for(var j=0;j<results.length;j++){ 
             
             var gifDiv = $("<div>");
-            var rating = results[j].rating;
+            var rating = results[j].rating.toUpperCase();
+            var pTitle = $('<p>').text('Title: ' + results[j].title.toUpperCase());
             var p = $("<p>").text("Rating: " + rating);
             var personImage = $("<img>");
+            personImage.addClass("s")
             personImage.attr("src", results[j].images.fixed_height.url);
-            gifDiv.append(p);
+            personImage.attr("data-animate", results[j].images.fixed_height.url);
+            personImage.attr("data-still", results[j].images.fixed_height_still.url);
+            
             gifDiv.append(personImage);
+            gifDiv.append(p);
+            gifDiv.append(pTitle);
             //here where we gonna manipulate the dom
-            $(".images").append(gifDiv); 
+            // $(".images").empty(); //this work but inly 1 image is displayed
+            $("#images").prepend(gifDiv); 
 
 
             }
@@ -51,16 +61,37 @@ $(document).ready(function () {
 
         var tag =$(".tag-input").val().trim();
         
-        $(".tag-input").val("")
-        tags.push(tag);
-        addButton();
-        searchGiphy();
+        $(".tag-input").val("");
+
+        if (tags.toString().toLowerCase().indexOf(tag.toLowerCase()) != -1) {
+            alert("Topic already exists");
+          }
+          else {
+            tags.push(tag);
+            // searchGiphy();
+            addButton();
+          }
+       
+        
+        
+        
     })
 
     
 
-      $(document).on("click",".w",searchGiphy);
+    $(document).on("click",".s", function() {
+      
+        var state = $(this).attr("data-state");
+        
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      }); 
+    
+    
 
-    addButton();
-    // searchGiphy();
 });
